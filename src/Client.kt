fd4195c9457522f86dda932f69b9c1837189380c
artem_admin <art241111@mail.ru>
2020-07-17 11:35:34 +0300

    Add try catch

--- src/Client.kt
+++ src/Client.kt
@@ -1,30 +1,68 @@
+import java.io.IOException
 import java.io.OutputStream
 import java.net.Socket
+import java.net.UnknownHostException
 import java.nio.charset.Charset
 import java.util.*
 import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
-    val address = "127.0.0.1"
-    val port = 9105
+    // Link param
+    val address = "localhost"
+    val port = 9999
 
+    // Create client
     val client = Client(address, port)
     client.run()
 }
 
 class Client(address: String, port: Int) {
-    private val connection: Socket = Socket(address, port)
-    private var connected: Boolean = true
+    private lateinit var connection: Socket
+    private var connected: Boolean
 
+    private lateinit var reader: Scanner
+    private lateinit var writer: OutputStream
+
+    /**
+     * Create connection and catch errors
+     */
     init {
-        println("Connected to server at $address on port $port")
+        try{
+            connection = Socket(address, port)
+            println("Connected to server at $address on port $port")
+
+            reader = Scanner(connection.getInputStream())
+            writer = connection.getOutputStream()
+
+            connected = true
+        }catch(e: IOException){
+            //TODO: Migrate to log
+            println(e)
+            connected = false
+        } catch (e: UnknownHostException){
+            //TODO: Migrate to log
+            println(e)
+            connected = false
+        }
     }
 
-    private val reader: Scanner = Scanner(connection.getInputStream())
-    private val writer: OutputStream = connection.getOutputStream()
-
+    /**
+     * Start connections
+     */
     fun run() {
-        thread { read() }
+        if(connected){
+            startRead()
+            startWrite()
+        }
+    }
+
+    private fun startRead(){
+        thread {
+            read()
+        }
+    }
+
+    private fun startWrite(){
         while (connected) {
             val input = readLine() ?: ""
             if ("exit" in input) {
@@ -35,7 +73,6 @@ class Client(address: String, port: Int) {
                 write(input)
             }
         }
-
     }
 
     private fun write(message: String) {
