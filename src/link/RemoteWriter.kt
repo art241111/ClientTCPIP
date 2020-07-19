c226320fc1e31cb9ac57901315cd01ef3d121626
art241111 <art241111@mail.ru>
2020-07-19 16:57:35 +0300

    Refactoring RemoteWriter.kte

--- src/link/RemoteWriter.kt
+++ src/link/RemoteWriter.kt
@@ -4,14 +4,14 @@ import protocols.Sender
 import java.io.PrintStream
 import java.net.Socket
 
-class RemoteWriter(private val socket: Socket): Sender {
+class RemoteWriter(socket: Socket): Sender {
     private var out: PrintStream = PrintStream(socket.getOutputStream())
 
     //this method writes to server, but waits no prompt.
     override fun write(message: String): Boolean {
         try {
-            out!!.println(message)
-            out!!.flush()
+            out.println(message)
+            out.flush()
             return true
         } catch (e: Exception) {
             e.printStackTrace()
