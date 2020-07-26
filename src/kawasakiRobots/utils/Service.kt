fbeab432eaaa5fce77789ed4c7ba9b00a755a8ce
art241111 <art241111@mail.ru>
2020-07-26 18:58:00 +0300

    Customization reset error

--- src/kawasakiRobots/utils/Service.kt
+++ src/kawasakiRobots/utils/Service.kt
@@ -2,6 +2,7 @@ package kawasakiRobots.utils
 
 import kawasakiRobots.commands.service.ServiceCommand.*
 import link.RobotEntity
+import link.State
 
 class Service(private var robotEntity: RobotEntity) {
     fun turnOnTheMotors() =
@@ -10,8 +11,12 @@ class Service(private var robotEntity: RobotEntity) {
     fun turnOffTheMotors() =
             robotEntity.writer.writeDependingStatus(TURN_OFF_THE_MOTORS.command)
 
-    fun resetErrors() =
-            robotEntity.writer.writeDependingStatus(DELETE_ERRORS.command)
+    fun resetErrors(): Boolean{
+        robotEntity.errors.clear()
+        robotEntity.state = State.WAITING_COMMAND
+        return robotEntity.writer.writeDependingStatus(DELETE_ERRORS.command)
+    }
+
 
     fun updateInfoAboutPosition() = robotEntity.writer
             .writeDependingStatus(ROBOT_POSITION.command)
